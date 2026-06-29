0e6b9977adced5442566f69365250d534a60901b
shremus <shreyash.musale@noc.ac.uk>
2026-06-29 16:52:29 +0100

    Generic function to download blob created for export endpoint

--- src/app/api/apiClient.ts
+++ src/app/api/apiClient.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosRequestConfig, AxiosResponse} from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_BROKERAGE_SERVICE_API ?? 'https://brokerage-service.paidiver.site';
@@ -12,10 +12,11 @@ const apiClient = axios.create({
 })
 
 interface RequestOptions extends Omit<AxiosRequestConfig, 'method' | 'url'> {
-    method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+    method: AxiosRequestConfig['method'];
     url: string;
     queryParams?: Record<string, string | number | boolean>;
     data?: unknown;
+    responseType?: 'json' | 'blob' | 'arraybuffer' | 'text';
 }
 
 export const apiRequest = async <T>({
@@ -23,6 +24,7 @@ export const apiRequest = async <T>({
     url,
     queryParams,
     data,
+    responseType,
     ...extraConfig}: RequestOptions
 ): Promise<T> => {
   const response: AxiosResponse<T> = await apiClient({
@@ -30,8 +32,47 @@ export const apiRequest = async <T>({
     url,
     params: queryParams,
     data: data,
+    responseType: responseType ?? 'json',
     ...extraConfig
   });
 
   return response.data;
+};
+
+
+interface DownloadProps extends Omit<AxiosRequestConfig, 'url' | 'responseType'> {
+  url: string;
+  defaultFilename?: string;
+}
+
+
+export const downloadFile = async ({
+    url,
+    defaultFilename = 'download.zip',
+    ...extraConfig
+}: DownloadProps): Promise<void> => {
+    try {
+        const fileBlob = await apiRequest<Blob>({
+        url,
+        method: 'GET',
+        responseType: 'blob',
+        ...extraConfig,
+        });
+
+        const downloadUrl = window.URL.createObjectURL(fileBlob);
+        const link = document.createElement('a');
+        
+        link.href = downloadUrl;
+        link.setAttribute('download', defaultFilename);
+        
+        document.body.appendChild(link);
+        link.click();
+        
+        link.remove();
+        window.URL.revokeObjectURL(downloadUrl);
+        
+    } catch (error) {
+        console.error(`Failed to download:`, error);
+        throw error;
+    }
 };
